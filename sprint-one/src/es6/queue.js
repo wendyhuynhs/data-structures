ae17522b75039c62c86934b13a38c038f698f4c6
Wendy Huynh <wendyhuynhs@gmail.com>
2019-04-10 12:38:03 -0700

    Completed es6 for stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  enqueue(val) {
+    this.storage[this.count] = val;
+    this.count++;
+  }
+  dequeue() {
+    this.count--;
+    var deleted = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[Number(key) - 1] = this.storage[this.count];
+    }
+    return deleted;
+  }
+  size() {
+    if (this.count < 0) {
+      return 0;
+    }
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    (this.storage = {}), (this.count = 0);
   }
 
+  push(val) {
+    this.storage[this.count] = val;
+    this.count++;
+  }
+  pop() {
+    this.count--;
+    var deleted = this.storage[this.count];
+    delete this.storage[this.count];
+    return deleted;
+  }
+  size() {
+    if (this.count < 0) {
+      return 0;
+    }
+    return this.count;
+  }
 }
