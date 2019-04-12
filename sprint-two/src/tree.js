a6b7fe0e97669b7690eafa848ccb6614f2ee7216
Wendy Huynh <wendyhuynhs@gmail.com>
2019-04-12 14:04:07 -0700

    Added traverse and remove from parent method (Advanced Section)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
-var Tree = function(value) {
+var Tree = function(value, parent) {
   // Time Complexity: Quadratic
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
   // your code here
   newTree.children = [];
@@ -11,6 +12,24 @@ var Tree = function(value) {
 };
 
 var treeMethods = {};
+
+treeMethods.removeFromParent = function(targetChildValue, node) {
+  var remove = false;
+  if (node.children) {
+    for (var i = 0; i < node.children.length; i++) {
+      if (node.children[i].value === targetChildValue) {
+        node.children[i].parent === null;
+        node.children.splice(i, 1);
+        return true;
+      } else if (children[i].children) {
+        remove = this.removeFromParent(node.children[i].children);
+      } else {
+        return true;
+      }
+    }
+  }
+  return remove;
+};
 
 treeMethods.addChild = function(value) {
   // Time Complexity - Constant
@@ -34,6 +53,17 @@ treeMethods.contains = function(target) {
   return false;
 };
 
+treeMethods.traverse = function(node, cb) {
+  var node = this;
+  cb(node.value);
+  if (node) {
+    if (node.children) {
+      for (var i = 0; i < node.children.length; i++) {
+        node.traverse(node.children[i], cb);
+      }
+    }
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
